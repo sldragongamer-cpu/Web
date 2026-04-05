199d78292cc97aaf0491c9a05927dba51a582736
Lithula Dilain <sldragongamer@gmail.com>
2026-04-05 12:46:01 +0530

    Refactor main.py to use Flask framework
    Refactor main.py to use Flask and add a home route.

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from flask import Flask
+from vercel_python_wsgi import handler
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+handler = handler(app)
+
 from Website  import create_app
 
 app = create_app()
